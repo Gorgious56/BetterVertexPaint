437ea2a145ec2d333390e85ca214d32ea2e64fd2
Nathan Hild <nathan.hild@gmail.com>
2020-10-11 15:42:37 +0200

    Housekeeping

--- EMVP/addon_preferences.py
+++ EMVP/addon_preferences.py
@@ -14,35 +14,29 @@ class BVPAddonPrefs(AddonPreferences):
     """
     bl_idname = __package__
 
-    color: bpy.props.FloatVectorProperty(
-        name="Color",
-        subtype='COLOR',
-        default=[1, 1, 1, 1],
-        size=4,
-        min=0,
-        max=1,
-    )
-
     map: bpy.props.EnumProperty(
         name="Map",
         items=all_maps,
         update=on_setting_change.update_prefs,
     )
+
     strength: bpy.props.FloatProperty(
         name="Strength",
-        default=1,
+        default=0,
         min=0,
         soft_max=1,
-        update=on_setting_change.update_prefs,
-    )
+        max=1,
         update=on_setting_change.update_prefs,
     )
 
+    subtract: bpy.props.BoolProperty(
+        default=False,
+        update=on_setting_change.update_prefs,
+    )
+
+
 addon_name = BVPAddonPrefs.bl_idname
-    def get_preferences(context):
-        return context.preferences.addons.get(
-            AddonPrefs.bl_idname).preferences
-    """
-    def draw(self, context):
-        layout = self.layout
-    """
+
+
+def get_preferences(context):
+    return context.preferences.addons.get(addon_name).preferences
--- EMVP/operators/add_pbr_material.py
+++ EMVP/operators/add_pbr_material.py
@@ -3,16 +3,89 @@ Operator to add a PBR based material using the right vertex color maps
 """
 
 import bpy
-from ..data.color_layers import layer_color_alpha, layer_msrt, layer_emi_ior, layer_sc_s
+from ..paint_logic import color_layers as layers
 
 
-class AddPBRMaterial(bpy.types.Operator):
+def create_nodes(material):
+    nodes = material.node_tree.nodes
+    nodes.clear()
+    links = material.node_tree.links
+    cols = [-300 - c * 200 for c in range(5)]
+
+    output = nodes.new(type='ShaderNodeOutputMaterial')
+    bsdf = nodes.new(type='ShaderNodeBsdfPrincipled')
+    bsdf.location = (cols[0], 0)
+
+    vc_color_alpha = nodes.new(type='ShaderNodeVertexColor')
+    vc_color_alpha.location = (cols[3], 0)
+
+    vc_subsurface_substrength = nodes.new(type='ShaderNodeVertexColor')
+    vc_subsurface_substrength.location = (cols[3], -100)
+
+    vc_msrt = nodes.new(type='ShaderNodeVertexColor')
+    vc_msrt.location = (cols[3], -200)
+    vc_msr_sep = nodes.new(type='ShaderNodeSeparateRGB')
+    vc_msr_sep.location = (cols[1], -170)
+
+    vc_b = nodes.new(type='ShaderNodeVertexColor')
+    vc_b.location = (cols[3], -500)
+    vc_b_sep = nodes.new(type='ShaderNodeSeparateRGB')
+    vc_b_sep.location = (cols[2], -400)
+    bump = nodes.new(type='ShaderNodeBump')
+    bump.location = (cols[1], -370)
+
+    vc_emi_emistr = nodes.new(type='ShaderNodeVertexColor')
+    vc_emi_emistr.location = (cols[3], -300)
+
+    vc_dirt = nodes.new(type='ShaderNodeVertexColor')
+    vc_dirt.location = (cols[3], -400)
+
+    dirt_mix = nodes.new(type='ShaderNodeMixRGB')
+    dirt_mix.location = (cols[2], 50)
+    dirt_mix.name = BVP_AddPBRMaterial.dirt_mix_name
+
+    vc_color_alpha.layer_name = layers.layer_color_alpha
+    links.new(vc_color_alpha.outputs[0], dirt_mix.inputs[1])  # Color
+    links.new(vc_color_alpha.outputs[1], bsdf.inputs[18])  # Alpha
+
+    vc_msrt.layer_name = layers.layer_msrt
+    # Separate Metallic, Specular, Roughness
+    links.new(vc_msrt.outputs[0], vc_msr_sep.inputs[0])
+    links.new(vc_msr_sep.outputs[0], bsdf.inputs[4])  # Metallic
+    links.new(vc_msr_sep.outputs[1], bsdf.inputs[5])  # Specular
+    links.new(vc_msr_sep.outputs[2], bsdf.inputs[7])  # Roughness
+    links.new(vc_msrt.outputs[1], bsdf.inputs[15]) # Transmission
+
+    vc_subsurface_substrength.layer_name = layers.layer_sc_s
+    links.new(vc_subsurface_substrength.outputs[0], bsdf.inputs[3])  # Subsurface color
+    links.new(vc_subsurface_substrength.outputs[1], bsdf.inputs[1])  # Subsurface Strength
+
+    vc_emi_emistr.layer_name = layers.layer_emi_emistr
+    links.new(vc_emi_emistr.outputs[0], bsdf.inputs[17])  # Emission
+
+    vc_b.layer_name = layers.layer_b
+    links.new(vc_b.outputs[0], vc_b_sep.inputs[0])
+    links.new(vc_b_sep.outputs[0], bump.inputs[2])
+    links.new(bump.outputs[0], bsdf.inputs[19])
+
+    vc_dirt.layer_name = layers.layer_dirt
+    links.new(vc_dirt.outputs[0], dirt_mix.inputs[2])
+
+    dirt_mix.blend_type = 'MULTIPLY'
+    dirt_mix.inputs[0].default_value = 1
+    links.new(dirt_mix.outputs[0], bsdf.inputs[0])
+
+    links.new(bsdf.outputs[0], output.inputs[0])  # Main output
+
+
+class BVP_AddPBRMaterial(bpy.types.Operator):
     """Set a PBR Material using the custom maps"""
-    bl_idname = "object.add_pbr_material"
+    bl_idname = "paint.bvp_set_pbr_material"
     bl_label = "Set PBR Material"
     bl_options = {'REGISTER', 'UNDO'}
 
     mat_name = "VC_PREVIEW"
+    dirt_mix_name = "Dirt_MIX"
 
     @classmethod
     def poll(cls, context):
@@ -24,50 +97,8 @@ class AddPBRMaterial(bpy.types.Operator):
         if vc_preview_mat is None:
             vc_preview_mat = bpy.data.materials.new(self.mat_name)
         vc_preview_mat.use_nodes = True
-        nodes = vc_preview_mat.node_tree.nodes
-        nodes.clear()
-        links = vc_preview_mat.node_tree.links
 
-        output = nodes.new(type='ShaderNodeOutputMaterial')
-        bsdf = nodes.new(type='ShaderNodeBsdfPrincipled')
-        bsdf.location = (-300, 0)
-
-        vc_color = nodes.new(type='ShaderNodeVertexColor')
-        vc_color.location = (-700, 0)
-
-        vc_msrt = nodes.new(type='ShaderNodeVertexColor')
-        vc_msrt.location = (-700, -200)
-        vc_msr_sep = nodes.new(type='ShaderNodeSeparateRGB')
-        vc_msr_sep.location = (-500, -170)
-
-        vc_subsurface = nodes.new(type='ShaderNodeVertexColor')
-        vc_subsurface.location = (-700, -100)
-
-        vc_emi_ior = nodes.new(type='ShaderNodeVertexColor')
-        vc_emi_ior.location = (-700, -300)
-
-        vc_color.layer_name = layer_color_alpha
-        links.new(vc_color.outputs[0], bsdf.inputs[0])  # Color
-        links.new(vc_color.outputs[1], bsdf.inputs[18])  # Alpha
-
-        vc_msrt.layer_name = layer_msrt
-        # Separate Metallic, Specular, Roughness
-        links.new(vc_msrt.outputs[0], vc_msr_sep.inputs[0])
-        links.new(vc_msr_sep.outputs[0], bsdf.inputs[4])  # Metallic
-        links.new(vc_msr_sep.outputs[1], bsdf.inputs[5])  # Specular
-        links.new(vc_msr_sep.outputs[2], bsdf.inputs[7])  # Roughness
-        links.new(vc_msrt.outputs[1], bsdf.inputs[15])  # Transmission
-
-        vc_subsurface.layer_name = layer_sc_s
-        links.new(vc_subsurface.outputs[0], bsdf.inputs[3])  # Subsurface color
-        # Subsurface strength
-        links.new(vc_subsurface.outputs[1], bsdf.inputs[1])
-
-        vc_emi_ior.layer_name = layer_emi_ior
-        links.new(vc_emi_ior.outputs[0], bsdf.inputs[17])  # Emission
-        # links.new(vc_emi_ior.outputs[1], bsdf.inputs[14])  # IOR
-
-        links.new(bsdf.outputs[0], output.inputs[0])  # Main output
+        create_nodes(vc_preview_mat)
 
         vc_preview_mat.blend_method = 'CLIP'
         vc_preview_mat.use_screen_refraction = True
