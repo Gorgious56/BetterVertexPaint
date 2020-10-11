0a18931bf6b7f1565bee07bd9d218322a823001d
Nathan Hild <nathan.hild@gmail.com>
2020-10-11 15:43:24 +0200

    Add class to register menus in correct order

---
+++ EMVP/menus/registerer.py
@@ -0,0 +1,34 @@
+"""
+Surrogate Class to register other menu items
+"""
+
+import bpy.types
+from . import bvp_main
+
+
+class VIEW3D_MT_registerer(bpy.types.Menu):
+    """
+    Surrogate Class to register other menu items
+    Do not use externally
+    """
+    bl_label = "surrogate menu registerer"
+
+    menus = (
+        bvp_main.pbr_material_submenu,
+        bvp_main.draw_brush_reminder,
+        bvp_main.draw_vertex_colors_reminder,
+        bvp_main.draw_material_reminder,
+        bvp_main.vertex_map_submenu,
+        bvp_main.vertex_color_strength_submenu,
+    )
+
+    def draw(self, context):
+        return
+
+    def register():
+        for menu in VIEW3D_MT_registerer.menus:
+            bpy.types.VIEW3D_MT_editor_menus.append(menu)
+
+    def unregister():
+        for menu in reversed(VIEW3D_MT_registerer.menus):
+            bpy.types.VIEW3D_MT_editor_menus.remove(menu)
